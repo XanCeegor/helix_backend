7490357dc0edca5d71ae16d556752ec6a475585e
Alex <alex.gouws@gmail.com>
2020-01-06 16:40:22 +0200

    created queued job to delete temp decrypted files after being added to archive file for download

---
+++ app/Jobs/DeleteTempFiles.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteTempFiles implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $files;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($files)
+    {
+        $this->files = $files;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach ($this->files as $file) {
+            Storage::delete($file->path);
+        }
+    }
+}
